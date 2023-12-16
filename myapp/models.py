6fc8140f6b50686fed16a0f37de25e0409613e07
unknown <ducdz1m2@gmail.com>
2023-12-16 13:34:39 +0700

    change16122023

--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Game(models.Model):
+    img = models.CharField(max_length=255)
+    title = models.CharField(max_length=255)
+    content = models.TextField(blank=True)
+    torrent = models.TextField(blank=True)
+    mega = models.TextField(blank=True)
--- myapp/views.py
+++ myapp/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
-
+from .models import Game
 # Create your views here.
 def home(request):
-
+    games = Game.objects.all()
     context = {
-
+        'games': games
     }
     return render(request, 'myapp/home.html', context)
